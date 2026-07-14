7bd497d8e58fc5cb66c93e73e7179c83c34fe1d8
Divya jain <divyjain2805@gmail.com>
2026-07-14 10:45:44 +0530

    Update cors for production

--- codewise-backend/src/app.js
+++ codewise-backend/src/app.js
@@ -23,7 +23,10 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: [
+    "http://localhost:5173",
+    "https://code-wise-tawny.vercel.app"
+  ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
